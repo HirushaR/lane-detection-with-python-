3fcb42926e7f2cfd5892b9d0d09a8f289e886524
HirushaR <hirusharandunu11@gmail.com>
2019-09-24 15:29:53 +0530

    sixth commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def make_cordinates(image, line_parameters):
@@ -18,9 +17,8 @@ def canny(image):
     return canny
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
-    if lines is not None :
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
